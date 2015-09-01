d1179e7e4b103cba25f6cd121be16f2e4a2c108b
tarasko79 <tarasko79@gmail.com>
2015-09-01 11:48:03 +0300

    hhh

---
+++ www/welcome.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @author tarasko 1.9.2015
+ *   
+ */
+
+    echo "велком";
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta http-equiv="ContentType" content="text/html" charset="utf-8"/>
+	<meta name="author" content="tarasko" />
+
+	<title>  </title>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
